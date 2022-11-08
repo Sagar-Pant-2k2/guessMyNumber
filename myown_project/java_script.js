0f9c60bd9c3505cde8933397a26e28368792489e
Sagar-Pant-2k2 <108977152+Sagar-Pant-2k2@users.noreply.github.com>
2022-11-08 19:02:55 +0530

    Add files via upload

---
+++ myown_project/java_script.js
@@ -0,0 +1,7 @@
+
+let secret_number=Math.floor(Math.random()*20+1);
+document.querySelector('#check_button').addEventListener('click',()=>{
+    console.log(document.querySelector('#number_input_box').value);
+});
+
+
